677fee80fba16ca6499a6f2964dd3d27e5959a9d
Void Main <voidmain1313113@gmail.com>
2013-03-24 22:13:59 +0800

    add config.ru for heroku

--- main.rb
+++ main.rb
@@ -7,19 +7,26 @@ require "uri"
 
 post '/github_hook' do
   push = JSON.parse(params[:payload])
+  puts push
   repo_name = push['repository']['name']
+  puts repo_name
   url = push['repository']['url']
+  puts url
   commiters = []
   push['commits'].each do |commit|
     name = commit['commiter']['name']
     commiters << name unless commiters.include?(name)
   end
+  puts commiters
 
   commiter_name = commiters.join(', ')
+  puts commiter_name
+
   message = "@all #{commiter_name} just pushed something to #{repo_name}, check it out here: #{url}"
+  puts message
 
   @config['message'] = message
-
+  puts @config
 
   url = "https://api.hipchat.com/v1/rooms/message?auth_token=#{@config['auth_token']}"
   uri = URI.parse(url)
